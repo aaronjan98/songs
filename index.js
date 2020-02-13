e6dee05223ca49862a6de7f9b9fb0152e4f70ddd
aaronjan <aaronjan98@gmail.com>
2020-02-13 10:36:13 -0800

    added more songs

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    const songs = [{ id: 1, name: "The Final Countdown" }];
+    const songs = [
+        { id: 1, name: "The Final Countdown" },
+        { id: 2, name: "Smells Like Teen Spirit" },
+        { id: 3, name: "Imagine" },
+        { id: 4, name: "One" },
+        { id: 5, name: "Billie Jean" },
+        { id: 6, name: "Bohemian Rhapsody" },
+        { id: 7, name: "Hey Jude" },
+        { id: 8, name: "Like A Rolling Stone" },
+        { id: 9, name: "I Can't Get No Satisfaction" }
+    ];
 
     res.status(200).json(songs);
 })
